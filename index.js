bdc36af278c4ae24140c52e585da5097af5fbf96
nnt3939 <nnt3939@gmail.com>
2024-03-09 12:05:15 +0900

    [add] first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+// 参考：https://ics.media/entry/14771/
+
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  const width = 500;
+  const height = 300;
+
+  // レンダラーを作成
+  const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector("#myCanvas"),
+  });
+  renderer.setSize(width, height);
+  renderer.setPixelRatio(window.devicePixelRatio); // デバイスピクセル比の設定
+
+  // シーンを作成
+  const scene = new THREE.Scene();
+
+//   // カメラを作成
+  const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
+//   カメラの初期座標を設定（X座標:0, Y座標:0, Z座標:0）
+  camera.position.set(0, 0, 1000);
+
+//   // 箱を作成
+  const geometry = new THREE.BoxGeometry(500, 500, 500);
+  const material = new THREE.MeshStandardMaterial({ color: 0x0000ff });
+  const box = new THREE.Mesh(geometry, material);
+  scene.add(box);
+
+//   // 平行光源
+  const light = new THREE.DirectionalLight(0xffffff);
+  light.intensity = 2; // 光の強さを倍に
+  light.position.set(1, 1, 1); // ライトの方向
+//   // シーンに追加
+  scene.add(light);
+
+//   // 初回実行
+  tick();
+
+  function tick() {
+    requestAnimationFrame(tick);
+
+    // 箱を回転させる
+    box.rotation.x += 0.01;
+    box.rotation.y += 0.01;
+
+    // レンダリング
+    renderer.render(scene, camera);
+}
+}
